7244a51d284f40c7d869ee72b18d125fd9b839e2
rrawann <rawan.sb@yahoo.com>
2025-08-20 16:04:22 +0300

    add all inventory defaults

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+
+{
+    'name': 'yousentech_inventory_default',
+    'category': 'Inventory',
+    'summary': """""",
+    'description': """""",
+    'author': 'yousen tech Techno Solutions, Odoo SA',
+    'website': "https://www.qimamhd.com",
+    'company': 'yousen Techno Solutions',
+    'maintainer': 'yousen Techno Solutions',
+    'depends': ['base', 'sale','purchase','account', 'stock','sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_user.xml',
+        'views/res_company.xml',
+        'views/purchase_order.xml',
+        'views/stock_picking.xml',
+         # 'views/sale_order.xml'
+    ],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'images': [],
+    'sequence': '-100',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
