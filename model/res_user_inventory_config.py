06823ba56996cac907b3f19dc5098c15dd3cbc03
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-16 15:00:10 +0300

    update

--- model/res_user_inventory_config.py
+++ model/res_user_inventory_config.py
@@ -64,7 +64,7 @@ class res_user_inventory_config(models.Model):
                 existing_records = self.search([
                     ('company_id', '=', record.company_id.id),
                     ('operation_type', '=', record.operation_type),
-                    ('id', '!=', record.id)
+                    ('id', '!=', record.id),('user_id','=',  self.env.uid)
                 ])
                 if existing_records:
                     raise ValidationError(
